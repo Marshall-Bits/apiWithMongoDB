38256e61aadeaae4b4da63cf4fe4ae568262d3d4
Marcelus <kz_@hotmail.es>
2023-08-04 14:20:14 +0200

    add get, post, delete routes
    Co-authored-by: YagoGu <YagoGu@users.noreply.github.com>
    Co-authored-by: evros13 <evros13@users.noreply.github.com>
    Co-authored-by: Erik Arvid Hosszú <ArvidErik@users.noreply.github.com>
    Co-authored-by: Óscar Mesejo <OscarMesejo@users.noreply.github.com>
    Co-authored-by: Juanda-85 <Juanda-85@users.noreply.github.com>
    Co-authored-by: Rodricancio8 <Rodricancio8@users.noreply.github.com>
    Co-authored-by: JoanLopezIT <JoanLopezIT@users.noreply.github.com>
    Co-authored-by: Hector  <TheXiShuai@users.noreply.github.com>
    Co-authored-by: German Panzardi <grpnzi@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,12 +1,52 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const User = require ('./user.model')
+const User = require('./user.model')
+
+
 
 mongoose.connect('mongodb+srv://marcel:12345@cluster0.vsfi8s4.mongodb.net/MyFirstDatabase')
     .then(() => console.log('mongoose connected'))
-    .catch((err)=> console.log('Mongo DB is not working..', err))
+    .catch((err) => console.log('Mongo DB is not working..', err))
 
 const app = express()
+
+app.get("/api/all", (req, res) => {
+    User.find()
+        .then((element) => {
+            console.log(element)
+            res.send(element)
+        })
+        .catch((err) => console.log("Cannot find users", err))
+});
+
+
+app.use(express.json()); // To parse JSON-encoded bodies
+
+app.post("/api/create", (req, res) => {
+    let data = req.body
+
+    const newUser = new User(data)
+    newUser.save()
+        .then((saveResponse) => {
+            console.log('User has been created', saveResponse)
+            res.send(saveResponse)
+        })
+        .catch((err) => console.log('User was not created', err))
+})
+
+app.delete("/api/delete", (req, res) => {
+    let data = req.body
+
+    User.findByIdAndDelete(data.id)
+    .then ((success) => {
+        console.log("element has been deleted", success)
+        res.send("element has been deleted")
+    })
+    .catch((err)=>{
+        res.send("error", err)
+    })
+});
+
 
 app.listen(3000, () => {
     console.log('server is running on port 3000...');
