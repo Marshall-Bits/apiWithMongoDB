22dd22202562ac67623b48cfe99451ed164677a3
Marcelus <kz_@hotmail.es>
2023-08-04 11:52:11 +0200

    add error

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb+srv://marcel:12345@cluster0.vsfi8s4.mongodb.net/MyFirstDatabase')
     .then(() => console.log('mongoose connected'))
-    .catch((err)=> console.log('Mongo DB is not working..'))
+    .catch((err)=> console.log('Mongo DB is not working..', err))
 
 
 const app = express()
